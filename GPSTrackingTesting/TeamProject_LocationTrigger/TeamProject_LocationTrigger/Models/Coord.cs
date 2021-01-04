b9baae09eb6703c405e52f40beca26b55cd3e14a
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-04 17:38:14 +0100

    Added Xamarin App to folder for GPS track testing.

---
+++ GPSTrackingTesting/TeamProject_LocationTrigger/TeamProject_LocationTrigger/Models/Coord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject_LocationTrigger.Models
+{
+    public class Coord
+    {
+        public double Latitude { get; set; }
+        public double Longtitude{ get; set; }
+
+        public Coord(double latitude, double longtitude){
+            Latitude = latitude;
+            Longtitude = longtitude;
+        }
+    }
+}
