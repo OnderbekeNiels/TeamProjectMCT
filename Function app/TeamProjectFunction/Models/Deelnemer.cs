1e4ad1fbca1eab0671d29343260288e06d7e687a
Xander <xander.petit@student.howest.be>
2021-01-06 09:15:35 +0100

    added deelnemer model

---
+++ Function app/TeamProjectFunction/Models/Deelnemer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProjectFunction.Models
+{
+    class Deelnemer
+    {
+        public Guid GebruikerId { get; set; }
+        public Guid RondeId { get; set; }
+    }
+}
