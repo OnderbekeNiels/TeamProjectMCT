9d4deedb43c69b85e8f5ee9419da41dad247ea81
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-07 19:25:52 +0100

    Geofencing With Shiny Geofencing
    Error 13: fixed by updating xamarin forms -> made it possible to enable background location in app.

---
+++ GPSTrackingTesting/ShinyGeolocationV2/ShinyGeolocationV2/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Shiny;
+using Shiny.Locations;
+using Shiny.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ShinyGeolocationV2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnStart.Clicked += BtnStart_Clicked;
+            btnStop.Clicked += BtnStop_Clicked;
+
+            // shiny doesn't usually manage your viewmodels, so we'll do this for now
+            var geofences = ShinyHost.Resolve<IGeofenceManager>();
+            var notifications = ShinyHost.Resolve<INotificationManager>();
+
+
+           
+            Register = new Command(async () =>
+            {
+
+               
+                // this is really only required on iOS, but do it to be safe
+                var access = await notifications.RequestAccess();
+                try
+                {
+                    if (access == AccessState.Available)
+                    {
+                        await geofences.StartMonitoring(new GeofenceRegion(
+                            "Home",
+                            new Position(50.91510937218809, 3.6512293828729945),
+                            Distance.FromMeters(10))
+                        {
+                            NotifyOnEntry = true,
+                            NotifyOnExit = true,
+                            SingleUse = false
+                        });
+                        Debug.WriteLine("Started Monitoring");
+                        lblMessage.Text = "Started Monitoring";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"ERROR: {ex.Message}");
+                }
+
+            });
+
+            Stop = new Command(async () =>
+            {
+                try
+                {
+                        await geofences.StopAllMonitoring();
+                        lblMessage.Text = "Stopped Monitoring";
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"ERROR: {ex.Message}");
+                }
+
+            });
+
+        }
+
+        public void NotifyMe()
+        {
+            lblMessage.Text = "ENTERED";
+        }
+
+        private void BtnStop_Clicked(object sender, EventArgs e)
+        {
+            Stop.Execute(null);
+        }
+
+        private void BtnStart_Clicked(object sender, EventArgs e)
+        {
+            //Toestemming vragen
+            AskPermitionsAsync();
+        }
+
+        public async Task AskPermitionsAsync()
+        {
+            var permission = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();
+            if (permission == PermissionStatus.Granted)
+            {
+                Register.Execute(null);
+            }
+            else if (permission != PermissionStatus.Granted)
+            {
+                permission = await Permissions.RequestAsync<Permissions.LocationAlways>();
+            }
+        }
+
+        public ICommand Register { get; }
+        public ICommand Stop { get; }
+    }
+
+}
+
---
+++ GPSTrackingTesting/ShinyGeolocationV2/ShinyGeolocationV2/Models/MyGeofenceDelegate.cs
@@ -0,0 +1,54 @@
+﻿using Shiny.Locations;
+using Shiny.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using ShinyGeolocationV2;
+using Xamarin.Forms;
+
+namespace ShinyGeolocationV2.Models
+{
+    public class MyGeofenceDelegate : IGeofenceDelegate
+    {
+        readonly INotificationManager notifications;
+
+        public MyGeofenceDelegate(INotificationManager notifications)
+        {
+            this.notifications = notifications;
+        }
+
+
+        public async Task OnStatusChanged(GeofenceState newStatus, GeofenceRegion region)
+        {
+            await Application.Current.MainPage.DisplayAlert(newStatus.ToString(), $"{region.Identifier}", "Ok");
+            Debug.WriteLine("Je bent binnen");
+
+            if (newStatus == GeofenceState.Entered)
+            {
+                Debug.WriteLine("Je bent binnen");
+                await this.notifications.Send(new Notification
+                {
+                    Title = "WELCOME!",
+                    Message = "It is good to have you back " + region.Identifier
+                });
+            }
+            else
+            {
+                await this.notifications.Send(new Notification
+                {
+                    Title = "GOODBYE!",
+                    Message = "You will be missed at " + region.Identifier
+                });
+            }
+        }
+
+
+
+        //Task IGeofenceDelegate.OnStatusChanged(GeofenceState newStatus, GeofenceRegion region)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
