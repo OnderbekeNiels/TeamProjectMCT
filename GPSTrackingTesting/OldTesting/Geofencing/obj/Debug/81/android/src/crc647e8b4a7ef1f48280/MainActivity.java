d11cfdbe0596578c3930e52be7d0544d2351be1f
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-07 10:06:07 +0100

    Added (working?) projects to repo for consult

---
+++ GPSTrackingTesting/OldTesting/Geofencing/obj/Debug/81/android/src/crc647e8b4a7ef1f48280/MainActivity.java
@@ -0,0 +1,75 @@
+package crc647e8b4a7ef1f48280;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.tasks.OnCompleteListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onStart:()V:GetOnStartHandler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onComplete:(Lcom/google/android/gms/tasks/Task;)V:GetOnComplete_Lcom_google_android_gms_tasks_Task_Handler:Android.Gms.Tasks.IOnCompleteListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"";
+		mono.android.Runtime.register ("Geofencing.MainActivity, Geofencing", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Geofencing.MainActivity, Geofencing", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onStart ()
+	{
+		n_onStart ();
+	}
+
+	private native void n_onStart ();
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void onComplete (com.google.android.gms.tasks.Task p0)
+	{
+		n_onComplete (p0);
+	}
+
+	private native void n_onComplete (com.google.android.gms.tasks.Task p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ GPSTrackingTesting/README.txt
@@ -0,0 +1,4 @@
+Geofencing: Sample van Microsoft Documentation die werkte.
+
+TeamProject_LocationTrigger:  Implementatie van libary: 
+https://github.com/CrossGeeks/GeofencePlugin
