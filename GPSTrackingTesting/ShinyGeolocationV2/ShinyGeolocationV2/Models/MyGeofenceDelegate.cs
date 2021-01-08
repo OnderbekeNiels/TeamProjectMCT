dab4457c11961ffbef114d425e73e6fdc6df815e
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-08 10:40:04 +0100

    Shiny Geofencing Event Handled
    Event wordt gecalled wanneer locatie voldoende opnieuw wordt opgevraagd

---
+++ GPSTrackingTesting/ShinyGeolocationV2/ShinyGeolocationV2/Models/MyGeofenceDelegate.cs
@@ -0,0 +1,36 @@
+﻿using Shiny.Locations;
+using Shiny.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using ShinyGeolocationV2;
+using Xamarin.Forms;
+using Shiny;
+
+namespace ShinyGeolocationV2.Models
+{
+    public class MyGeofenceDelegate : IGeofenceDelegate, IShinyStartupTask
+    {
+
+        readonly CoreDelegateServices services;
+        public MyGeofenceDelegate(CoreDelegateServices services) => this.services = services;
+
+
+        public async Task OnStatusChanged(GeofenceState newStatus, GeofenceRegion region)
+        {
+            Debug.WriteLine("Ca marche wi matje");
+            await this.services.Notifications.Send(
+                this.GetType(),
+                newStatus == GeofenceState.Entered,
+                "Geofence Event",
+                $"{region.Identifier} was {newStatus}"
+            );
+        }
+
+
+        public void Start()
+            => this.services.Notifications.Register(this.GetType(), true, "Geofences");
+    }
+}
---
+++ GPSTrackingTesting/ShinyGeolocationV2/ShinyGeolocationV2/Startup.cs
@@ -0,0 +1,26 @@
+﻿using ShinyGeolocationV2.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+using Shiny.Logging;
+namespace ShinyGeolocationV2
+{
+    public class Startup : ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection builder)
+        {
+            // custom logging
+            Log.UseConsole();
+            Log.UseDebug();
+
+            // create your infrastructure
+            //builder.AddSingleton<SampleSqliteConnection>();
+
+            //// register all of the acr stuff you want to use
+            //builder.UseGpsBackground<>();
+            builder.AddSingleton<AppNotifications>();
+            builder.AddSingleton<CoreDelegateServices>();
+            builder.UseGeofencing<MyGeofenceDelegate>();
+            builder.UseNotifications(true);
+        }
+    }
+}
