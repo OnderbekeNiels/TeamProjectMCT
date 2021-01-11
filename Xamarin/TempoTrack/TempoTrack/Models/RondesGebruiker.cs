f6df304ecf159f517b53a1b7e3fd97c4762b7b48
Jelle <jelle.demets@student.howest.be>
2021-01-11 17:07:34 +0100

    RondeOverzichtPagina, bijna af

---
+++ Xamarin/TempoTrack/TempoTrack/Models/RondesGebruiker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TempoTrack.Models
+{
+    public class RondesGebruiker
+    {
+        public DateTime StartDatum { get; set; }
+        public string RondeNaam { get; set; }
+        public Guid RondeId { get; set; }
+        public int Ranking { get; set; }
+
+        public override string ToString()
+        {
+            return $"Startdatum: {StartDatum}, RondeNaam: {RondeNaam}, RondeId: {RondeId}, Ranking: {Ranking}";
+        }
+    }
+}
