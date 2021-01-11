b3ba4d1586af8f1c2dda24bb75c6ab8efb907b15
Robbe <robbe.raevens@student.howest.be>
2021-01-11 15:00:21 +0100

    Front end buttons arrow up / down

---
+++ Xamarin/TempoTrack/TempoTrack/Views/EtappePaginas/CreateEtappe.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TempoTrack.Views.EtappePaginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateEtappe : ContentPage
+    {
+        public CreateEtappe()
+        {
+            InitializeComponent();
+        }
+    }
+}
