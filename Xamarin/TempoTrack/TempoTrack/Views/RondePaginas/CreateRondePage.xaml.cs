073c10127466444972e62037c2a285800fa6510a
Robbe <robbe.raevens@student.howest.be>
2021-01-11 10:37:37 +0100

    CreateRonde page Backend (Error pop up nog maken)

--- Xamarin/TempoTrack/TempoTrack/Views/RondePaginas/CreateRondePage.xaml.cs
+++ Xamarin/TempoTrack/TempoTrack/Views/RondePaginas/CreateRondePage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TempoTrack.Models;
+using TempoTrack.Repositories;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,22 @@ namespace TempoTrack.Views.RondePaginas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreateRondePage : ContentPage
     {
+        GebruikerV2 gebruiker;
         public CreateRondePage(GebruikerV2 gebruikersInfo)
         {
             InitializeComponent();
+            btnCreate.Clicked += btnCreate_cliked;
+            gebruiker = gebruikersInfo;
         }
+
+        private void btnCreate_cliked(object sender, EventArgs e)
+        {
+            Ronde ronde = new Ronde();
+            ronde.Admin = gebruiker.GebruikerId;
+            ronde.Naam = entRondeNaam.Text;
+
+            RondeRepository.CreateRonde(ronde);
+        }
+
     }
 }
