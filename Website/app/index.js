c3444e81eb902edfd2abfa20eb5af8cb6424b782
Xander <xander.petit@student.howest.be>
2021-01-11 13:58:55 +0100

    added redirects and sessionstotage

--- Website/app/index.js
+++ Website/app/index.js
@@ -1,15 +1,51 @@
-    function onSignIn(googleUser) {
+var userId;
+
+function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        GetUserId(profile);
   }
 
+const GetUserId = async function(profile){
+  let endpoint = `https://temptrackingfunction.azurewebsites.net/api/gebruikers/login?code=WJ/wMMoTjMGaF6AdEBO9gyjfMaODsitooxxbpAavwzUhEj4WcgrLqw==`;
+  let data  = {"name": `${profile.getName}`, "email": `${profile.getEmail}`};
 
-  function signOut() {
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+  //Then with the data from the response in JSON...
+  .then((data) => {
+    //console.log('Success:', data);
+    userId = data["gebruikerId"];
+    console.log(userId);
+    sessionStorage.setItem("gebruikerId", userId);
+    window.location.pathname = '/roundsoverview.html'
+  })
+  //Then with the error genereted...
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+
+function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
+      window.location.pathname = '/index.html'
     });
   }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("DOM loaded");
+}
+)
