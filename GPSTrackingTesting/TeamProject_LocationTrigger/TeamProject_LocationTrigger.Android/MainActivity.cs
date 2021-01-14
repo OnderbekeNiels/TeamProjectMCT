8401963574d41d4902ccd1ad970e36c1c5827cc4
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-14 19:59:04 +0100

    Afwerking test project GPS tracking
    Commentaar incl

--- GPSTrackingTesting/TeamProject_LocationTrigger/TeamProject_LocationTrigger.Android/MainActivity.cs
+++ GPSTrackingTesting/TeamProject_LocationTrigger/TeamProject_LocationTrigger.Android/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.Widget;
 using Android.OS;
 using Android.Content;
 using TeamProject_LocationTrigger.Models;
+using Plugin.Geofence;
 
 namespace TeamProject_LocationTrigger.Droid
 {
@@ -28,48 +29,10 @@ namespace TeamProject_LocationTrigger.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
 
-            //Set the app context for the geofencing
-            AppContext = this.ApplicationContext;
-            //TODO: Initialize CrossGeofence Plugin
-            //TODO: Specify the listener class implementing IGeofenceListener interface in the Initialize generic
-            //CrossGeofence.Initialize<CrossGeofenceListener>();
-            //CrossGeofence.GeofenceListener.OnAppStarted();
-            //Start a sticky service to keep receiving geofence events when app is closed.
-            // URL to this Plugin's Documentation: https://github.com/CrossGeeks/GeofencePlugin
-            StartService();
         }
 
-        /// <summary>
-        /// Function that is used by geofencing plugin to start the geofencing service for android
-        /// </summary>
-        public static void StartService()
-        {
-            AppContext.StartService(new Intent(AppContext, typeof(GeofenceService)));
 
-            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.Kitkat)
-            {
-
-                PendingIntent pintent = PendingIntent.GetService(AppContext, 0, new Intent(AppContext, typeof(GeofenceService)), 0);
-                AlarmManager alarm = (AlarmManager)AppContext.GetSystemService(Context.AlarmService);
-                alarm.Cancel(pintent);
-            }
-        }
-
-        /// <summary>
-        /// Function that is used to stop geofencing on android platform
-        /// </summary>
-        public static void StopService()
-        {
-            AppContext.StopService(new Intent(AppContext, typeof(GeofenceService)));
-            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.Kitkat)
-            {
-                PendingIntent pintent = PendingIntent.GetService(AppContext, 0, new Intent(AppContext, typeof(GeofenceService)), 0);
-                AlarmManager alarm = (AlarmManager)AppContext.GetSystemService(Context.AlarmService);
-                alarm.Cancel(pintent);
-            }
-        }
-
-    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
