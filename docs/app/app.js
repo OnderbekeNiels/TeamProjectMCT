28f4146b5e381ec2e9e1c36bd7e31e1da3fcc974
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-19 09:03:04 +0100

    Opkuisen console.log en tables

--- docs/app/app.js
+++ docs/app/app.js
@@ -357,7 +357,6 @@ const showEtappeUserData = function (data) {
 const showEtappeUserChartData = function (data) {
   let converted_labels = [];
   let converted_data = [];
-  console.log(data);
   for (const item of data) {
     converted_labels.push(item.lapNummer);
     converted_data.push(item.tijdLap);
@@ -486,7 +485,6 @@ const getEtappesRanking = async function (etappeId) {
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
-    console.table(data);
     showEtappesRanking(data);
     getEtappeInfo(etappeId);
   } catch (error) {
@@ -499,7 +497,6 @@ const getEtappeInfo = async function (etappeId) {
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
-    console.table(data);
     showEtappeInfo(data);
   } catch (error) {
     console.error("An error occured, we handled it.", error);
@@ -511,7 +508,6 @@ const getEtappeUserData = async function (etappeId) {
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
-    console.table(data);
     showEtappeUserData(data);
   } catch (error) {
     console.error("An error occured, we handled it.", error);
@@ -523,7 +519,6 @@ const getEtappeUserChartData = async function (etappeId) {
   try {
     const response = await fetch(endpoint);
     const data = await response.json();
-    console.table(data);
     showEtappeUserChartData(data);
   } catch (error) {
     console.error("An error occured, we handled it.", error);
@@ -535,17 +530,17 @@ const getEtappeUserChartData = async function (etappeId) {
 //#region *** Google Authentication ***
 
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // var profile = googleUser.getBasicProfile();
+  // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log("Name: " + profile.getName());
+  // console.log("Image URL: " + profile.getImageUrl());
+  // console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log("User signed out.");
+    // console.log("User signed out.");
     localStorage.removeItem("gebruikerId");
     localStorage.removeItem("name");
     window.location.pathname = "/index.html";
@@ -555,7 +550,6 @@ function signOut() {
 //#endregion
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM loaded :)");
 
   //Listen to click logo -> go to home
   listenToClickLogo();
@@ -565,8 +559,6 @@ document.addEventListener("DOMContentLoaded", function () {
   userName = localStorage.getItem("name");
 
   if (userId == null || userName == null) {
-    console.log(userId);
-    console.log(userName);
     window.location.pathname = "/index.html";
   } else {
 
