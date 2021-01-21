7b4e4a0a6a5fb8700382d195d8f5bfd6bb191058
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-21 09:56:45 +0100

    Added 2 functions for admin on website

---
+++ Function app/TeamProjectFunction/Models/GebruikerRelated/RondesAdmin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProjectFunction.Models.GebruikerRelated
+{
+    public class RondesAdmin
+    {
+        public Guid RondeId { get; set; }
+        public string RondeNaam { get; set; }
+        public DateTime StartDatum { get; set; }
+        public int AantalEtappes { get; set; }
+        public Guid Admin { get; set; }
+    }
+}
