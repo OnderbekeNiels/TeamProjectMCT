9cc33ef67d2be9ca9642fad478b5c7dcb544a54b
Jelle <jelle.demets@student.howest.be>
2021-01-22 12:07:45 +0100

    DeelnemersOverzicht, EtappeOverzicht gefixed

--- Xamarin/TempoTrack/TempoTrack/Views/RondePaginas/DeelnemersOverzichtPage.xaml.cs
+++ Xamarin/TempoTrack/TempoTrack/Views/RondePaginas/DeelnemersOverzichtPage.xaml.cs
@@ -15,21 +15,44 @@ namespace TempoTrack.Views.RondePaginas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DeelnemersOverzichtPage : ContentPage
     {
-        private static Guid RondeId;
-        public DeelnemersOverzichtPage(Guid rondeId)
+        private static RondesGebruiker RondeInfo { get; set; }
+        private static GebruikerV2 GebruikersInfo { get; set; }
+
+        bool admin = false;
+        public DeelnemersOverzichtPage(RondesGebruiker rondeInfo, GebruikerV2 gebruikersInfo)
         {
+            RondeInfo = rondeInfo;
+            GebruikersInfo = gebruikersInfo;
+
             checkConnectivity();
-            //Kleuren instellen voor navbar
-            NavigationPage.SetHasBackButton(this, true);
-            Color fireRed = Color.FromHex("#B22222");
-            ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = fireRed;
-            ((NavigationPage)Application.Current.MainPage).BarTextColor = Color.White;
-
-            RondeId = rondeId;
-
             InitializeComponent();
 
-            btnRefresh.Clicked += BtnRefresh_Clicked;
+
+            if (GebruikersInfo.GebruikerId == RondeInfo.Admin)
+            {
+                //Kleuren instellen voor navbar
+                NavigationPage.SetHasBackButton(this, true);
+                Color fireRed = Color.FromHex("#B22222");
+                ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = fireRed;
+                ((NavigationPage)Application.Current.MainPage).BarTextColor = Color.White;
+                btnRefresh.TextColor = fireRed;
+                btnRefresh.BorderColor = fireRed;
+                admin = true;
+            }
+            else
+            {
+                //Kleuren instellen voor navbar
+                //NavigationPage.SetHasBackButton(this, true);
+                Color Blauw = Color.FromHex("#015D99");
+                ((NavigationPage)Application.Current.MainPage).BarBackgroundColor = Blauw;
+                ((NavigationPage)Application.Current.MainPage).BarTextColor = Color.White;
+                btnRefresh.TextColor = Blauw;
+                btnRefresh.BorderColor = Blauw;
+                admin = false;
+            }
+
+
+                btnRefresh.Clicked += BtnRefresh_Clicked;
 
             LoadDeelnemersAsync();
         }
@@ -38,10 +61,19 @@ namespace TempoTrack.Views.RondePaginas
         {
             checkConnectivity();
             int teller = 0;
-            List<DeelnemersRonde> deelnemers = await RondeRepository.GetDeelnemersRonde(RondeId);
+            List<DeelnemersRonde> deelnemers = await RondeRepository.GetDeelnemersRonde(RondeInfo.RondeId);
 
             foreach(DeelnemersRonde item in deelnemers)
             {
+                if(admin == true)
+                {
+                    item.Kleur = "#B22222";
+                }
+                else
+                {
+                    item.Kleur = "#015D99";
+                }
+
                 teller += 1;
                 item.Nummer = teller;
             }
