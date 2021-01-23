e935835331a9a86dd61e526810d02b6ae0e013a4
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-01-23 17:32:14 +0100

    Stop etappe bug fixed

---
+++ Function app/TeamProjectFunction/Models/GebruikerRelated/LapControle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProjectFunction.Models.GebruikerRelated
+{
+    public class LapControle
+    {
+        public int AantalRegistarties { get; set; }
+        public int Laps { get; set; }
+    }
+}
